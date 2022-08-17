e995f6c961af56b50d448b41119cda41da727f9f
bzhack404 <110977767+bzhack404@users.noreply.github.com>
2022-08-17 12:09:35 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KSR|CE-Attendance system||ksrce.com </title>
+     <link rel="shortcut icon" href="/energy.png" type="image/x-icon">
+    <!-- <link rel="stylesheet" href="style.mime">   -->
+     <link rel="stylesheet" href="/style.css">
+     <link href='https://css.gg/css' rel='stylesheet'>
+      <!-- <script src="/js/node.js" type="java"></script> -->
+</head>
+<body>
+   <div class="tab"> </div>
+    <form action="/" method="POST">
+    <div>
+    <h1 style="color: blue;">Login</h1><br>
+    <p>E-mail</p>
+    <input type="text" name="username"  placeholder="user" >
+    <!-- <span class="in1">User</span> -->
+    <p>Password</p>
+    <input type="text" name="password"  placeholder="pass" >
+    <!-- <span class="in2"> Pass</span> -->
+    <br><input class="login" type="submit"  value="submit"> 
+    <a href="" style="text-decoration:none;" class="ic1">contectus for any detials</a>
+    <a href=""><i class="gg-facebook"></i></a>
+    <a href=""><i class="gg-twitter"></i></a>
+    <a href=""><i class="gg-mail"></i></a>
+    <br>
+    </div>
+    </form>
+    <div class="tab2"> </div>   
+ <!-- <script>
+    function myFunction() {
+        var x = document.getElementById("user").value;
+        var y = document.getElementById("pass").value;
+       // console.log(x);
+       // console.log(y);
+    }
+ </script> -->
+</body>
+</html>
